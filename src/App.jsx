66dcada130fa3e3c384ea32baa19b11910e8a535
musa sheriff <venom@kali.venom>
2025-05-31 00:02:15 +0100

    env and plugins added

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,28 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import ReactLogo from './assets/react.svg?react'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './components/Header'
 
+console.log('All env vars:', import.meta.env);
+console.log('Greeting:', import.meta.env.VITE_GREETING);
 function App() {
+  const greeting = import.meta.env.VITE_GREETING
   const [count, setCount] = useState(0)
 
   return (
     <>
+     <Header />
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <ReactLogo className="logo react" alt="React logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
+      <h2>{greeting}</h2>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+console.log('Header.jsx is loading');
+
+export default function Header() {
+    return(
+        <header>
+            <h1>Hello World whats up</h1>
+        </header>
+    )
+}
